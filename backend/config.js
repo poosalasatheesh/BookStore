1bb85550ef6a482326ae9c828ef9b115cdee31ca
satheeshpoosala <poosalasatheeshkumar@gmail.com>
2024-04-11 18:24:59 +0530

    configchanges

--- backend/config.js
+++ backend/config.js
@@ -1,12 +1,12 @@
 export const PORT = 5010;
 
 export var dbURL =
-  'mongodb+srv://satheesh:satheesh@mycluster.eyy1gge.mongodb.net/BookstoreDb';
+  'mongodb+srv://satheesh:satheesh@eshop.rbm4kue.mongodb.net/BookstoreDb';
+
+// mongodb+srv://satheesh:satheesh@eshop.rbm4kue.mongodb.net/
 
 // 'mongodb+srv://satheesh-Kumar:Satheesh123@book-store-db.gjrmlfn.mongodb.net/';
 
 // mongodb+srv://satheesh:satheesh@mycluster.eyy1gge.mongodb.net/BookstoreDb
 
 //mongodb+srv://<username>:<password>@mycluster.eyy1gge.mongodb.net/
-
-
