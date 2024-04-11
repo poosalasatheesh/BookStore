a3e72e98626f817d26404b61f40f3f2b809d5083
satheeshpoosala <poosalasatheeshkumar@gmail.com>
2024-04-11 23:31:24 +0530

    backIndex.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,7 @@
 import express from 'express';
-import { PORT, dbURL } from './config.js';
 import mongoose from 'mongoose';
+import { PORT, dbURL } from './config.js';
 // import { Book } from './Models/BookModel.js';
-import router from './routes/booksRoute.js';
 import cors from 'cors';
 // var cors = require('cors');
 // var express = require('express') commented bcz module import is done  in  package.json type:module
@@ -11,22 +10,23 @@ import cors from 'cors';
 var app = express();
 app.use(express.json());
 app.use(cors());
-app.use('/books', router);
 //server run in port
-app.listen(PORT, () => {
-  console.log(`Server started in port `);
-});
 
 app.get('/', (req, res) => {
   res.send('hello how are  you iam from backend');
 });
 
+import router from './routes/booksRoute.js';
+app.use('/books', router);
 //How  to store BookData to DB
 
 mongoose
   .connect(dbURL)
   .then(() => {
     console.log('DB CONNECTED');
+    app.listen(PORT, () => {
+      console.log(`Server started in port `);
+    });
   })
   .catch((error) => {
     console.log(error);
